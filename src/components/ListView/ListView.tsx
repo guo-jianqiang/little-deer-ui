9719f739814cd6538236b1c2b63fdc2150cf9e74
guojianqiang <guojianqiang@cecdat.com>
2020-08-25 21:16:09 +0800

    update listview

--- src/components/ListView/ListView.tsx
+++ src/components/ListView/ListView.tsx
@@ -5,6 +5,22 @@ export interface PropsType {
     style?: object;
     className?: string;
     /**
+     * 加载完成文本
+     */
+    loadedText?: string;
+    /**
+     * 是否加载完成
+     */
+    isLoaded?: boolean;
+    /**
+     * 加载完成文本
+     */
+    loadingText?: string;
+    /**
+     * 加载中dom
+     */
+    loadingEle?: any;
+    /**
      * 调用onLoad之前的临界值，单位是像素
      * @default 10
      */
@@ -15,25 +31,28 @@ export interface PropsType {
     onLoad?: (currentPage?: number) => Promise<boolean>;
 }
 
-class ListView extends Component<PropsType, {}> {
+class ListView extends Component<PropsType, {completed: boolean}> {
     static defaultProps = {
-        onEndReachedThreshold: 10
+        onEndReachedThreshold: 10,
+        loadedText: '没有更多了',
+        loadingText: '加载中...'
     }
     constructor(props: PropsType, context) {
         super(props, context);
+        this.state = {
+            completed:true
+        }
     }
-    completed: boolean = true
     currentPage: number = 1
 
     onScroll = (e) => {
-        if (e.target.scrollHeight - e.target.scrollTop - e.target.clientHeight < 10 && this.completed) {
-            this.completed = false
+        if (e.target.scrollHeight - e.target.scrollTop - e.target.clientHeight < 10 && this.state.completed) {
+            this.setState({completed: false})
             if (typeof this.props.onLoad === 'function') {
                 this.props.onLoad(this.currentPage).then(res => {
                     if (res) {
-                        this.completed = true
+                        this.setState({completed: true})
                         this.currentPage++
-                        console.log(123)
                     }
                 })
             }
@@ -47,6 +66,21 @@ class ListView extends Component<PropsType, {}> {
                 onScroll={this.onScroll}
             >
                 {this.props.children}
+                {
+                    !this.props.isLoaded ?
+                        this.props.loadingEle ? this.props.loadingEle
+                            : !this.state.completed  && <div className='list-view-loading'>
+                                <div className='list-view-loading-icon' />
+                                {
+                                    this.props.loadingText
+                                }
+                            </div>
+                        : <div className='list-view-loading'>
+                            {
+                                this.props.loadedText
+                            }
+                        </div>
+                }
             </div>
         );
     }
